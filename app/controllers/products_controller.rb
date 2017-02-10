97887a4559392485d42f1d41e8f89bc88cd009da
Matt Eichsteadt <matteichsteadt@gmail.com>
2017-02-10 11:22:52 -0800

    set up navigation and index page

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,52 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @products = Product.all
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @products = Product.all
+    @product = Product.new
+  end
+
+  def create
+    @products = Product.all
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @products = Product.all
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @products = Product.all
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @products = Product.all
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :price, :country, :descriptions, :images)
+  end
+end
